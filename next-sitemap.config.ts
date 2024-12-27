2e488c7016ddd5bcf0da21e213c35b1fb056f53e
giuliodepascale <depascale8@gmail.com>
2024-12-27 18:39:49 +0100

    aggiunta site map

---
+++ next-sitemap.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://eventlyitalia.com', // Cambia con il dominio del tuo sito
+    generateRobotsTxt: true, // Genera automaticamente un file robots.txt
+    sitemapSize: 5000, // Numero massimo di URL per sitemap (opzionale)
+  };
+  
+  module.exports = config;
+  
