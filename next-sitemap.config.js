ca11e0038dc83a5c1291d23f745928a12ca9b143
giuliodepascale <depascale8@gmail.com>
2024-12-27 18:51:56 +0100

    fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-    siteUrl: 'https://eventlyitalia.com', // Cambia con il dominio del tuo sito
-    generateRobotsTxt: true, // Genera automaticamente un file robots.txt
-    sitemapSize: 5000, // Numero massimo di URL per sitemap (opzionale)
-  };
-  
-  module.exports = config;
-  
+  siteUrl: 'https://eventlyitalia.com', // Cambia con il tuo dominio
+  generateRobotsTxt: true, // Per generare automaticamente robots.txt
+  generateIndexSitemap: true, // Crea l'indice della sitemap
+};
+
+module.exports = config;
