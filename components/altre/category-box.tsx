dfe206bf9b692493f2d556186b6113c4ca54dba8
giuliodepascale <depascale8@gmail.com>
2025-01-11 17:06:39 +0100

    fix velocità category boc

--- components/altre/category-box.tsx
+++ components/altre/category-box.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useCallback } from 'react';
 import { IconType } from 'react-icons';
 
 interface CategoryBoxProps {
@@ -14,37 +13,24 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({ icon: Icon, label, selected }
   const router = useRouter();
   const params = useSearchParams();
 
-  const handleClick = useCallback(() => {
+  const handleClick = () => {
     const currentParams = new URLSearchParams(params?.toString());
-
     if (params?.get('category') === label) {
       currentParams.delete('category');
     } else {
       currentParams.set('category', label);
     }
+    router.replace(`/?${currentParams.toString()}`); // Usa replace per evitare un re-render completo
+  };
 
-    const url = `/?${currentParams.toString()}`;
-    router.push(url);
-  }, [label, params, router]);
+  const classes = `
+    flex flex-col items-center justify-center gap-2 p-3 border-b-2 transition cursor-pointer 
+    ${selected ? 'border-b-neutral-800 text-neutral-800' : 'border-transparent text-neutral-500'}
+    hover:text-neutral-800
+  `;
 
   return (
-    <div
-      onClick={handleClick}
-      className={`
-        flex
-        flex-col
-        items-center
-        justify-center
-        gap-2
-        p-3
-        border-b-2
-        hover:text-neutral-800
-        transition
-        cursor-pointer
-        ${selected ? 'border-b-neutral-800' : 'border-transparent'}
-        ${selected ? 'text-neutral-800' : 'text-neutral-500'}
-      `}
-    >
+    <div onClick={handleClick} className={classes}>
       <Icon size={26} />
       <div className="font-medium text-sm">{label}</div>
     </div>
