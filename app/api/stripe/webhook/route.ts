010796d6a60be41db77a3813bf8d58935f473c12
giuliodepascale <depascale8@gmail.com>
2025-03-10 20:58:39 +0100

    fix

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
-import { updateOrganizationTicketingStatus } from '@/lib/stripe';
+
 
 export const config = { api: { bodyParser: false } };
 
