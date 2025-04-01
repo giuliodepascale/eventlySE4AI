1aa16a0828c92554ce695a6747c8c15a825be727
giuliodepascale <depascale8@gmail.com>
2025-04-01 21:31:53 +0200

    refactor(auth): remove Google OAuth and related components
    Removed Google OAuth provider and associated components (`Social.tsx`) to simplify the authentication flow. Deleted unused migrations and API endpoints related to IP geolocation. Updated the Prisma schema to remove the `Account` model and its relations. These changes improve maintainability and reduce unnecessary code.

--- auth.config.ts
+++ auth.config.ts
@@ -1,17 +1,13 @@
-//import Google from "next-auth/providers/google"
+
 import type { NextAuthConfig } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import { LoginSchema } from "./schemas"
 import { getUserByEmail } from "./data/user";
 import bcrypt from "bcryptjs";
-import Google from "next-auth/providers/google"
+
 
 
 export default { providers: [
-    Google({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
     Credentials
     ({
         async authorize(credentials) {
